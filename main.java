173cc8bcd4921d9bc7b974346961281b85c59f0f
riti2601 <ritishapande@gmail.com>
2024-04-16 12:00:33 +0530

    start

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class main{
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.setTitle("Special Number");
+    }
+}
