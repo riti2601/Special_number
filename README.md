c295ac19bc4e1bddeb1588324c59dd47b48d1d79
riti2601 <ritishapande@gmail.com>
2024-04-16 12:11:37 +0530

    design

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Special_number
+A Java Swing code where you can input a number and see if its any of the following special number like - Armstrong Number, Palindrome number, Magic numeber etc.
--- main.java
+++ main.java
@@ -1,12 +1,30 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class main{
     public static void main(String[] args) {
         
+        JButton find = new JButton();
+        find.setText("Find");
+        find.setBounds(200,250,100,50);
+        find.setFocusable(false);
+
+        JPanel panel1 = new JPanel();
+        panel1.setBounds(0,0,500,250);
+        panel1.setBackground(Color.DARK_GRAY);
+        panel1.setLayout(null);
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setVisible(true);
         frame.setTitle("Special Number");
+        frame.setLayout(null);
+        frame.add(panel1);
+        frame.add(find);
+
     }
 }
